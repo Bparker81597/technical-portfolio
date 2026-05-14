82b882d841a28737f9775299fea6f38adfae5df3
Nathan Baker <nathan.baker@atomicobject.com>
2026-05-14 09:54:05 -0400

    Revamp Parker Tech Lab multi-page site

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        work: resolve(__dirname, 'work.html'),
+        interactive: resolve(__dirname, 'interactive.html'),
+        contact: resolve(__dirname, 'contact.html'),
       },
     },
     copyPublicDir: true,
